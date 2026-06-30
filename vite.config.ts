4ff88a82ec7decbc3e7db5be48e5b5a2e9e5659c
procokiegod <134407707+procokiegod@users.noreply.github.com>
2026-07-01 00:23:15 +0200

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// If your site is at username.github.io/repo-name/ change base to '/repo-name/'
-// If using a custom domain or username.github.io/ leave as '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/shadow-client/",
 });
-
